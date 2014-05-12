5c74e800019725292f3d12b47df31bf77c0fc285
Alex Chadin <alexchadin@Alexs-iMac.local>
2014-05-12 22:35:59 +0400

    first commit

---
+++ js/chess.js
@@ -0,0 +1,228 @@
+var socket = io.connect('http://194.58.100.119:8080');
+//Board creating
+function CreateBoard (boardHeight, boardWidth) {
+	var board = $('#board');
+	for (var i = 0; i < boardHeight; i++) {
+		for (var j = 0; j < boardWidth; j++) {
+			if ((i % 2 == 0 && j % 2 == 0) || (i % 2 != 0 && j % 2 != 0)) {
+				board.append ($('<div class="lightCell"> </div>').attr("x",i).attr("y",j));
+			} else {
+				board.append ($('<div class="darkCell"> </div>').attr("x",i).attr("y",j));
+            }
+		}
+	}
+}
+
+function Insert (x,y,figure) {
+    $('[x='+x+']'+'[y='+y+']').append(figure);//$([x=''][y=''])
+}
+
+//Figures dotting
+function Dotting () {
+    cell = $('.darkCell, .lightCell');
+
+    //pawns
+    var blackPawn = '<img color="black" type="pawn" isFirstMove="true" src="'+pathToDark+' P.ico">';
+    var whitePawn = '<img color="white" type="pawn" isFirstMove="true" src="'+pathToLight+' P.ico">';
+    //rooks
+    var blackRook = '<img color="black" type="rook" src="' + pathToDark + ' R.ico">';
+    var whiteRook = '<img color="white" type="rook" src="' + pathToLight + ' R.ico">';
+    //knights
+    var blackKnight = '<img color="black" type="knight" src="' + pathToDark + ' N.ico">';
+    var whiteKnight = '<img color="white" type="knight" src="' + pathToLight + ' N.ico">';
+    //bishops
+    var blackBishop = '<img color="black" type="bishop" src="' + pathToDark + ' B.ico">';
+    var whiteBishop = '<img color="white" type="bishop" src="' + pathToLight + ' B.ico">';
+    //queens
+    var blackQueen = '<img color="black" type="queen" src="' + pathToDark + ' Q.ico">';
+    var whiteQueen = '<img color="white" type="queen" src="' + pathToLight + ' Q.ico">';
+    //kings
+    var blackKing = '<img color="black" type="king" src="' + pathToDark + ' K.ico">';
+    var whiteKing = '<img color="white" type="king" src="' + pathToLight + ' K.ico">';
+
+    for (var i = 0; i < 64; i++) {
+        cell.eq(i).attr('id',i);         //putting id to cells
+    }
+
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            //pawns
+            if (i == 1) {
+                Insert(i,j,blackPawn);
+            } else if (i == 6) {
+                Insert (i,j,whitePawn);
+            }
+            //first row
+            else if (i == 0) {
+                if (j == 0 || j == 7)  {
+                    Insert(i,j, blackRook);
+                }
+                else if (j == 1 || j == 6) {
+                    Insert(i,j,blackKnight);
+                }
+                else if (j == 2 || j == 5) {
+                    Insert (i, j, blackBishop);
+                }
+                else if (j == 3) {
+                    Insert (i, j, blackQueen);
+                } else if (j == 4) {
+                    Insert(i,j,blackKing);
+                }
+            }
+            //eigth row
+            else if (i == 7) {
+                if (j == 0 || j == 7)  {
+                    Insert(i,j, whiteRook);
+                }
+                else if (j == 1 || j == 6) {
+                    Insert(i,j,whiteKnight);
+                }
+                else if (j == 2 || j == 5) {
+                    Insert (i, j, whiteBishop);
+                }
+                else if (j == 3) {
+                    Insert (i, j, whiteQueen);
+                } else if (j == 4) {
+                    Insert(i,j,whiteKing);
+                }
+            }
+        }
+    }
+}
+
+function IsEmpty (cell) {
+    if ($(cell).find('img').length == 0) return true;
+    return false;
+};
+function IsOurs(cell){
+	if ($(cell).attr('color')==activeColor.current)
+	{ 
+	return true;
+	}
+	return false;
+};
+
+socket.on('moveFigure', function(obj){
+	var figure = $('[x='+obj.x2+']'+'[y='+obj.y2+']').children();
+	var c = $('[x='+obj.x1+']'+'[y='+obj.y1+']')
+    if (obj.result == true) {
+		if (obj.ccolor != activeColor.current)
+			changeColor();
+		$(figure).parent().toggleClass('checked');
+		$(c).append(figure);
+		$('[x=' + obj.x2 + ']' + '[y=' + obj.y2 + ']').empty();
+    }else{
+    	$(figure).parent().toggleClass('checked');
+    }
+});
+
+$(function() {
+    $('#accept').click(function () {
+        var message = {
+            userid:$('#namebox').val(),
+            roomid:$('#roombox').val()
+        }
+        $('#roombox').val('');
+        $('#namebox').val('');
+        socket.emit('auth', message);
+    });
+    $('#reset').click(function () {
+    	var message = {m:'reset'};
+    	socket.emit('reset_board', message);
+    	location.reload();
+    });
+});
+socket.on('attackFigure', function(obj){
+	var figure = $('[x='+obj.x2+']'+'[y='+obj.y2+']').children();
+	var c = $('[x='+obj.x1+']'+'[y='+obj.y1+']')
+	if (obj.type=="knight"){
+		if (obj.result==true){
+			if (obj.ccolor!=activeColor.current)
+				changeColor();
+			$(figure).parent().toggleClass('checked');
+			$(c).empty();
+			$(c).append(figure);
+		}
+		else{
+			$(figure).parent().toggleClass('checked');
+		}
+	}
+    if (obj.type=="pawn"|| obj.type=="rook" || obj.type=="bishop" || obj.type=="queen" || obj.type=="king"){
+        if (obj.result=="Fire"){
+            if (obj.ccolor!=activeColor.current)
+                changeColor();
+            $(figure).parent().toggleClass('checked');
+            $(c).empty();
+            $(c).append(figure);
+        }
+        else{
+        	$(figure).parent().toggleClass('checked');
+        }
+    }
+});
+function Move (figure) {
+	if ($(figure).attr('color')==activeColor.current){
+    	//alert($(figure).parent().attr('x')+' '+ $(figure).parent().attr('y'));
+    	$(figure).parent().toggleClass('checked');
+    	var figureID = $(figure).parent().attr('id');
+    	cell.unbind('click').click(function() {//this - cell
+			var newID = $(this).attr('id');
+			console.log(newID);
+			var newID = $(this).attr('id');
+			var cx = $(this).attr('x');
+			var cy = $(this).attr('y');
+			var fx = $(figure).parent().attr('x');
+			var fy = $(figure).parent().attr('y');
+			var f_type = $(figure).attr('type');
+			var ob = {
+					x1:cx, 
+					y1:cy, 
+					x2:fx, 
+					y2:fy,
+					type:f_type,
+					ccolor:activeColor.current
+					};
+			if (IsEmpty(this)){
+				console.log(cx+' '+cy+' '+fx+' '+fy);
+				socket.emit('sendMove', ob);
+				figure=null;
+				}
+			else if ($(this).children().attr('color')!=activeColor.current && $(this).children().length!=0){
+				socket.emit('sendAttack', ob);
+				figure=null;
+			}else{
+				$(figure).parent().toggleClass('checked');
+			}
+    	});
+    }
+}
+
+var activeColor = {
+	black:"black",
+	white:"white",
+	current:"white",
+	enemy:"black"
+	};
+function changeColor() {
+	if (activeColor.current==activeColor.white){
+		activeColor.current=activeColor.black;
+		activeColor.enemy=activeColor.white;
+	}else{
+		activeColor.current=activeColor.white;
+		activeColor.enemy=activeColor.black;
+	}
+}
+//пути к иконкам
+pathToLight = 'figures/light/White';
+pathToDark = 'figures/dark/Black';
+$(document).ready(function () {
+	CreateBoard (8,8);
+	Dotting();
+    figure ='img'; //клик только по белым
+    $('#board').on('click', figure, function(){
+        //console.log ('IsEmpty:'+IsEmpty(this));
+        clFigure = this;        
+        //alert($(this).parent().attr('x')+' '+ $(this).parent().attr('y'));
+        Move(clFigure);
+	});
+});
---
+++ server.js
@@ -0,0 +1,504 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+
+// configuration settings for PORT
+app.set('port', process.env.PORT || 8080);
+
+// configure routing
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+app.use("/js", express.static(__dirname+'/js'));
+app.use("/style", express.static(__dirname+'/style'));
+app.use("/figures", express.static(__dirname+'/figures'));
+
+var server = http.createServer(app).listen(app.get('port'), function() {
+  console.log("Express listening on port: " + app.get('port'));
+});
+var activeColor = {
+	black:"black",
+	white:"white",
+	current:"white",
+	enemy:"black"
+	};
+function changeColor() {
+	if (activeColor.current==activeColor.white){
+		activeColor.current=activeColor.black;
+		activeColor.enemy=activeColor.white;
+	}else{
+		activeColor.current=activeColor.white;
+		activeColor.enemy=activeColor.black;
+	}
+}
+function Figure(color, type, isFirstMove){
+	this.color = color;
+	this.type = type;
+	this.isFirstMove = isFirstMove;
+}
+function Cell(x,y, child){
+	this.x=x;
+	this.y=y;
+	this.child=child;
+	this.ifFree=false;
+	child.parent = this;
+	if (child.color==' '){
+		this.isFree = true;
+	}
+}
+
+var blackPawn =   new Figure("b", "p", 'true');
+var whitePawn =   new Figure("w", "p", 'true');
+var blackRook =   new Figure("b", "r");
+var whiteRook =   new Figure("w", "r");
+var blackKnight = new Figure("b", "k");
+var whiteKnight = new Figure("w", "k");
+var blackBishop = new Figure("b", "b");
+var whiteBishop = new Figure("w", "b");
+var blackQueen =  new Figure("b", "q");
+var whiteQueen =  new Figure("w", "q");
+var blackKing =   new Figure("b", "K");
+var whiteKing =   new Figure("w", "K");
+function Init(){
+	blackPawn =   new Figure("b", "p", 'true');
+	whitePawn =   new Figure("w", "p", 'true');
+	blackRook =   new Figure("b", "r");
+	whiteRook =   new Figure("w", "r");
+	blackKnight = new Figure("b", "k");
+	whiteKnight = new Figure("w", "k");
+	blackBishop = new Figure("b", "b");
+	whiteBishop = new Figure("w", "b");
+	blackQueen =  new Figure("b", "q");
+	whiteQueen =  new Figure("w", "q");
+	blackKing =   new Figure("b", "K");
+	whiteKing =   new Figure("w", "K");
+}
+var Zero = new Figure(' ',' ');
+var n = 8, m = 8;
+var mas = [];
+var out = [];
+function dot(){
+	for (var i=0;i<n;i++){
+		mas[i]=[];
+	}
+	mas[0][0] = new Cell(0,0,blackRook);
+	mas[0][7] = new Cell(0,7,blackRook);
+	mas[7][0] = new Cell(7,0,whiteRook);
+	mas[7][7] = new Cell(7,7,whiteRook);
+	mas[0][1] = new Cell(0,1,blackKnight);
+	mas[0][6] = new Cell(0,6,blackKnight);
+	mas[7][1] = new Cell(7,1,whiteKnight);
+	mas[7][6] = new Cell(7,6,whiteKnight);
+	mas[0][2] = new Cell(0,2,blackBishop);
+	mas[0][5] = new Cell(0,5,blackBishop);
+	mas[7][2] = new Cell(7,2,whiteBishop);
+	mas[7][5] = new Cell(7,5,whiteBishop);
+	mas[0][3] = new Cell(0,3,blackQueen);
+	mas[7][3] = new Cell(7,3,whiteQueen);
+	mas[0][4] = new Cell(0,4,blackKing);
+	mas[7][4] = new Cell(7,4,whiteKing);
+	for (var i=0;i<n;i++){
+		mas[1][i] = new Cell(1,i,new Figure("b","p",'true'));
+	}
+	for (var i=0;i<n;i++){
+		mas[6][i] = new Cell(6,i,new Figure("w","p",'true'));
+	}
+	for (var i=2;i<6;i++){
+		for (var j=0;j<m;j++){
+			mas[i][j]= new Cell(i,j,Zero);
+		}
+	}
+}
+function isEmpty(_x,_y){
+	if (mas[_x][_y].child.color==' '){
+		return true;
+	}
+	return false;
+}
+function Move(cx,cy,fx,fy){
+	mas[cx][cy].child = mas[fx][fy].child;
+	mas[fx][fy].child = new Figure(' ',' ');
+	toString();
+}
+function toString(){
+	for (var i = 0; i < m; i++){
+		out[i] = [];
+	}
+	for (var i=0;i<n;i++){
+		for (var j=0;j<m;j++){
+			out[i][j] = mas[i][j].child.color+''+mas[i][j].child.type;
+		}
+	}
+}
+function ResetBoard(){
+	Init();
+	dot();
+	toString();
+	console.log(out);
+}
+ResetBoard();
+
+/**
+ * @return {string}
+ * @return {string}
+ */
+function CanPawn(_cx, _cy, _fx, _fy, color, isf){
+	var cx = parseInt(_cx);
+	var cy = parseInt(_cy);
+	var fx = parseInt(_fx);
+	var fy = parseInt(_fy);
+    var movemax=isf;
+	if (color=='white'){
+		if (cy==fy){
+			if (fx-cx<=movemax && fx-cx>0){
+				while (fx>cx){
+					fx--;
+					if ((fx==cx)&&(fy==cy)){
+						return true;
+					}else if (!isEmpty(fx,fy)){
+						return false;
+					}
+				}
+			}
+		}
+        else if ((fx-1==cx && fy+1==cy)||(fx-1==cx && fy-1==cy)){
+            return "Fire";
+        }
+	}
+    else if (color=='black'){
+        if (cy==fy){
+            if (cx-fx<=movemax&& cx-fx>0){
+                while (cx>fx){
+                    fx++;
+                    if ((fx==cx)&&(fy==cy)){
+                        return true;
+                    }else if (!isEmpty(fx,fy)){
+                        return false;
+                    }
+                }
+            }
+        }
+        else if ((fx+1==cx && fy+1==cy)||(fx+1==cx && fy-1==cy)){
+            return "Fire";
+        }
+    }
+}
+function CanRookStep(_cx, _cy, _fx, _fy) {
+	var cx = parseInt(_cx);
+	var cy = parseInt(_cy);
+	var fx = parseInt(_fx);
+	var fy = parseInt(_fy);
+	var foundFigure=false;
+	var canMove = false;
+	var canAttack = false;
+	if ((cy==fy)&&(cx!=fx)){
+		if (cx>fx){
+			while (cx>fx){
+				fx++;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}else if (cx<fx) {
+			while (cx<fx){
+				fx--;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}
+	}
+	else if ((cy!=fy)&&(cx==fx)){
+		if (cy>fy){
+			while (cy>fy) {
+				fy++;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}else if (cy<fy) {
+			while (cy<fy){
+				fy--;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}
+	}
+	if (foundFigure)return false;
+	else if (canMove&&canAttack) return "Fire";
+	else if (canMove) return true;
+}
+function CanKnightStep(_cx,_cy,_fx,_fy){
+	var cx = parseInt(_cx);
+	var cy = parseInt(_cy);
+	var fx = parseInt(_fx);
+	var fy = parseInt(_fy);
+	console.log(cx+' '+cy+' '+fx+' '+fy);
+
+	if (((fx-1==cx)&&(fy+2==cy))||
+		((fx+1==cx)&&(fy+2==cy))||
+		((fx+2==cx)&&(fy+1==cy))||
+		((fx+2==cx)&&(fy-1==cy))||
+		((fx+1==cx)&&(fy-2==cy))||
+		((fx-1==cx)&&(fy-2==cy))||
+		((fx-2==cx)&&(fy-1==cy))||
+		((fx-2==cx)&&(fy+1==cy))) return true;
+	return false;
+}
+function CanBishopStep(_cx,_cy,_fx,_fy){
+	var cx = parseInt(_cx);
+	var cy = parseInt(_cy);
+	var fx = parseInt(_fx);
+	var fy = parseInt(_fy);
+	var foundFigure=false;
+	var canMove = false;
+	var canAttack = false;
+	if (cx<fx){
+		if (cy>fy){
+			while ((cx<fx)&&(cy>fy)){
+				fy++;
+				fx--;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}else if (cy<fy){
+			while ((cx<fx)&&(cy<fy)){
+				fx--;
+				fy--;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}
+	}else if (cx>fx){
+		if (cy>fy){
+			while ((cx>fx)&&(cy>fy)){
+				fx++;
+				fy++;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}else if (cy<fy){
+			while ((cx>fx)&&(cy<fy)){
+				fx++;
+				fy--;
+				if ((fx==cx)&&(fy==cy)){
+					canMove=true;
+					if (mas[fx][fy].child.color==activeColor.enemy.charAt(0)){
+						canAttack=true;
+					}
+				}
+				else if (mas[fx][fy].child.color!=' '){
+					foundFigure=true;
+				}
+			}
+		}
+	}
+	if (foundFigure)return false;
+	else if (canMove&&canAttack) return "Fire";
+	else if (canMove) return true;
+}
+function CanQueenStep(_cx,_cy,_fx,_fy){
+	var cx = parseInt(_cx);
+	var cy = parseInt(_cy);
+	var fx = parseInt(_fx);
+	var fy = parseInt(_fy);
+	if (CanBishopStep(cx, cy, fx, fy)=="Fire" || CanRookStep(cx, cy, fx, fy)=="Fire")return "Fire";
+	if (CanBishopStep(cx, cy, fx, fy)==true || CanRookStep(cx, cy, fx, fy)==true)return true;
+	return false;
+}
+function CanKingStep(_cx,_cy,_fx,_fy){
+	var cx = parseInt(_cx);
+	var cy = parseInt(_cy);
+	var fx = parseInt(_fx);
+	var fy = parseInt(_fy);
+	if ((mas[fx][fy].child.color==activeColor.enemy.charAt(0))&&((fx-cx==1&&fy-cy==0)||
+		(fx-cx==1&&fy-cy==-1)||
+		(fx-cx==0&&fy-cy==-1)||
+		(fx-cx==-1&&fy-cy==-1)||
+		(fx-cx==-1&&fy-cy==0)||
+		(fx-cx==-1&&fy-cy==1)||
+		(fx-cx==0&&fy-cy==1)||
+		(fx-cx==1&&fy-cy==1))){
+		return "Fire";
+	}
+	if ((fx-cx==1&&fy-cy==0)||
+		(fx-cx==1&&fy-cy==-1)||
+		(fx-cx==0&&fy-cy==-1)||
+		(fx-cx==-1&&fy-cy==-1)||
+		(fx-cx==-1&&fy-cy==0)||
+		(fx-cx==-1&&fy-cy==1)||
+		(fx-cx==0&&fy-cy==1)||
+		(fx-cx==1&&fy-cy==1)) return true;
+	return false;
+}
+var io = require('socket.io').listen(server);
+io.set('log level', 0);
+var rooms = {};
+io.sockets.on('connection', function(socket) {
+	socket.on('auth', function(message){
+		rooms[message.roomid] = message.roomid;
+        rooms[message.roomid][message.userid] = message.userid;
+        console.log(rooms[message.roomid]+' '+rooms[message.roomid][message.userid]);
+	});
+	socket.on('reset_board', function(message){
+		if (message.m=='reset'){
+			ResetBoard();
+		}
+	});
+	socket.on('sendMove', function(obj){
+        var result;
+		if (obj.type=="knight"){
+			result = CanKnightStep(obj.x1,obj.y1,obj.x2,obj.y2);
+		}
+		if (obj.type=="pawn"){
+            if (mas[obj.x2][obj.y2].child.isFirstMove=='true') {
+                result = CanPawn(obj.x1, obj.y1, obj.x2, obj.y2, obj.ccolor, 2);
+            }else{
+                result = CanPawn(obj.x1, obj.y1, obj.x2, obj.y2, obj.ccolor, 1);
+            }
+		}
+		if (obj.type=="rook"){
+			result = CanRookStep(obj.x1, obj.y1, obj.x2, obj.y2);
+		}
+		if (obj.type=="bishop"){
+			result = CanBishopStep(obj.x1, obj.y1, obj.x2, obj.y2);
+		}
+		if (obj.type=="queen"){
+			result = CanQueenStep(obj.x1, obj.y1, obj.x2, obj.y2);
+		}
+		if (obj.type=="king"){
+			result = CanKingStep(obj.x1, obj.y1, obj.x2, obj.y2);
+		}
+		if (result==true){
+		    mas[obj.x2][obj.y2].child.isFirstMove='false';
+			Move(obj.x1,obj.y1,obj.x2,obj.y2);
+			console.log(out);
+			activeColor.current=obj.ccolor;
+			changeColor();
+			obj.ccolor=activeColor.current;
+		}
+		obj.result=result;
+		io.sockets.emit('moveFigure', obj);
+		
+	});
+	
+	socket.on('sendAttack', function(obj){
+		var result;
+		if (obj.type=="knight"){
+			result = CanKnightStep(obj.x1,obj.y1,obj.x2,obj.y2);
+			if (result){
+				Move(obj.x1,obj.y1,obj.x2,obj.y2);
+				console.log(out);
+				activeColor.current=obj.ccolor;
+				changeColor();
+				obj.ccolor=activeColor.current;
+
+			}
+		}
+        if (obj.type=="pawn"){
+            if (mas[obj.x2][obj.y2].child.isFirstMove=='true') {
+                result = CanPawn(obj.x1, obj.y1, obj.x2, obj.y2, obj.ccolor, 2);
+            }else{
+                result = CanPawn(obj.x1, obj.y1, obj.x2, obj.y2, obj.ccolor, 1);
+            }
+            if (result=="Fire"){
+            	if (mas[obj.x2][obj.y2].child.hasOwnProperty('ifFirstMove'))
+            		mas[obj.x2][obj.y2].child.isFirstMove='false';
+                Move(obj.x1,obj.y1,obj.x2,obj.y2);
+                console.log(out);
+                activeColor.current=obj.ccolor;
+				changeColor();
+				obj.ccolor=activeColor.current;
+            }
+        }
+        if (obj.type=="rook"){
+        	result = CanRookStep(obj.x1, obj.y1, obj.x2, obj.y2);
+        	if (result=="Fire"){
+        		Move(obj.x1,obj.y1,obj.x2,obj.y2);
+				console.log(out);
+				activeColor.current=obj.ccolor;
+				changeColor();
+				obj.ccolor=activeColor.current;
+        	}
+        }
+        if (obj.type=="bishop"){
+        	result = CanBishopStep(obj.x1, obj.y1, obj.x2, obj.y2);
+        	if (result=="Fire"){
+        		Move(obj.x1,obj.y1,obj.x2,obj.y2);
+				console.log(out);
+				activeColor.current=obj.ccolor;
+				changeColor();
+				obj.ccolor=activeColor.current;
+        	}
+        }
+        if (obj.type=="queen"){
+        	result = CanQueenStep(obj.x1, obj.y1, obj.x2, obj.y2);
+        	if (result=="Fire"){
+        		Move(obj.x1,obj.y1,obj.x2,obj.y2);
+				console.log(out);
+				activeColor.current=obj.ccolor;
+				changeColor();
+				obj.ccolor=activeColor.current;
+        	}
+        }
+        if (obj.type=="king"){
+        	result = CanKingStep(obj.x1, obj.y1, obj.x2, obj.y2);
+        	if (result=="Fire"){
+        		Move(obj.x1,obj.y1,obj.x2,obj.y2);
+				console.log(out);
+				activeColor.current=obj.ccolor;
+				changeColor();
+				obj.ccolor=activeColor.current;
+        	}
+        }
+        obj.result=result;
+        io.sockets.emit('attackFigure', obj);
+
+	});
+});
